535d87eff72d36256d03f5f5435cefb67ea9a03d
Nandoliya <ahsanali.nandoliya@capgemini.com>
2024-01-31 12:35:52 +0530

    SCRUM-6 inital project setup

---
+++ jira_app/src/main/java/com/jira/genai/jira/GenAiJiraApplication.java
@@ -0,0 +1,13 @@
+package com.jira.genai.jira;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenAiJiraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenAiJiraApplication.class, args);
+	}
+
+}
---
+++ jira_app/src/test/java/com/jira/genai/jira/GenAiJiraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jira.genai.jira;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenAiJiraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
